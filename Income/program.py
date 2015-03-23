5463c96a66d11939f5b50666022f1977c76a434c
senghuot <senghuotlim@gmail.com>
2015-03-22 23:16:14 -0700

    First commit

---
+++ Income/program.py
@@ -0,0 +1,8 @@
+from numpy import *
+
+def main():
+  training_tmp = genfromtxt('data.txt')
+  print 'hello world'
+
+if __name__ == '__main__':
+  main()
---
+++ README.md
@@ -0,0 +1,1 @@
+Contains various Machine Learning projects
